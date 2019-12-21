c22ffc46f5ca9c9ecd5fd1ae1b5df92ec0bc0c3b
RW21 <rinze.watanabe.translation@gmail.com>
2019-12-21 16:37:44 +0900

    Update

--- sierpinski.py
+++ sierpinski.py
@@ -24,27 +24,50 @@ def random_point_inside_triangle(triangle):
     return [np.sqrt(r1) * (1 - r2), r2 * np.sqrt(r1)]
 
 
-def chaos_game(n):
-    triangle = np.array([[0, 0], [1000, 0], [0, 1000]])
+def chaos_game_triangle(n):
+    triangle = np.array([[0, 0], [2, 0], [1, 1]])
 
     points_x = []
     points_y = []
-    points = []
 
     position = random_point_inside_triangle(triangle)
 
     for i in range(n):
         position = ((triangle[np.random.randint(0, 3)] - position)) / 2
+        # print(position)
         points_x.append(position[0])
         points_y.append(position[1])
-        # points.append(position)
 
-    # plt.figure(dpi=1200)
-    # plt.scatter(points_x, points_y, s=0.5)
     plt.scatter(points_x, points_y, s=0.5)
+    plt.savefig('sample_3.png', dpi=1000)
 
-    plt.savefig('sample_1.png', dpi=1200)
+
     plt.show()
 
 
-chaos_game(20000)
+def random_point_inside_square(square):
+    square_side = square[1][0] - square[0][0]
+    return [np.random.uniform(0, square_side), np.random.uniform(0, square_side)]
+
+
+def chaos_game_square(n):
+    square = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
+    points_x = []
+    points_y = []
+
+    position = random_point_inside_square(square)
+
+    for i in range(n):
+        print(position)
+        position = np.absolute((square[np.random.randint(0, 4)] - position)) * (3/4)
+        points_x.append(position[0])
+        points_y.append(position[1])
+
+    plt.scatter(points_x, points_y, s=0.5)
+    plt.show()
+
+
+
+
+chaos_game_triangle(20000)
+# chaos_game_square(10000)
